405dac92ca1bf863ed5ce35d8c8c4a54e4dac974
aramsing <aramsin2@hotmail.com>
2025-08-13 19:19:42 -0500

    Establishing separate pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
+import React from 'react';
+import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <>
-      <h1>Hello! Welcome to my website!</h1>
-      <h2>My name is Arjun Ramsinghani and I am a Software Engineer.</h2>
-    </>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
   )
 }
-
-export default App
---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+            <h1>This page {'('}and the entire website{')'} is under construction.</h1>
+        </>
+    )
+}
