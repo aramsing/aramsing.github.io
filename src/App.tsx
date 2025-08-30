32d01574c9cf2bc03b2f5b0f6015e864f857a64e
aramsing <aramsin2@hotmail.com>
2025-08-30 18:13:28 -0500

    displaying picture

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <>
       <h2>Hi, I'm Arjun Ramsinghani</h2>
-      <img src="./src/assets/1707169444707.jpg" alt="Arjun Ramsinghani at the Texas Tech Engineering Job Fair"/>
+      <img src="/1707169444707.jpg" alt="Arjun Ramsinghani at the Texas Tech Engineering Job Fair"/>
       <p>Welcome to my website! I am a Software Engineer at USAA.</p>
     </>
   )
