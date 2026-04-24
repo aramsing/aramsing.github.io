d4a7a7c79aef63bba44ca8bd1a32c0a63d39363e
aramsing <aramsin2@hotmail.com>
2026-04-24 18:17:19 -0500

    added goals

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,21 @@ export default function App() {
       <h2>Hi, I'm Arjun Ramsinghani</h2>
       <img src="/1707169444707.jpg" alt="Arjun Ramsinghani at the Texas Tech Engineering Job Fair"/>
       <p>Welcome to my website! I am a Software Engineer at USAA.</p>
+
+        <h5>Pages I want to make</h5>
+        <ul style={{alignItems: 'flex-start', justifyContent: 'flex-start'}}>
+            <li>Education</li>
+            <li>Work</li>
+            <li>About (outside work)</li>
+        </ul>
+
+
+        <h5>Technical things I want to do:</h5>
+        <ul style={{alignItems: 'flex-start', justifyContent: 'flex-start'}}>
+            <li>Convert to React</li>
+            <li>Showcase some prior projects</li>
+            <li>The goal for this website is to act a a complementary portfolio to my already existing resume</li>
+        </ul>
     </>
   )
 }
